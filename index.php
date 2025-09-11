308ce394fe7dd7e9fc0c3d853f0cb78efa56699c
Tobias-Giertz <tobias.giertz@gmail.com>
2025-09-11 12:35:14 +0200

    index drop down

--- index.php
+++ index.php
@@ -8,7 +8,12 @@ echo "Hello! Please enter the details of the movie you've watched.";
 <form action="insert.php" method="POST">
     Movie Name:<input type="text" name="mname"><br>
     Movie Year:<input type="text" name="myear"><br>
-    Movie Genre:<input type="text" name="mgenre"><br>       
+    Movie Genre:
+    <select name="mgenre" id="mgenre">
+        <option value="1">Action/Adventure</option>
+        <option value="2">Drama</option>
+    </select>
+    <br>  
     Movie Rating:<input type="text" name="mrating"><br>
     <input type="submit" value="Add Movie">
 </form>
