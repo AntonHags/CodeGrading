851d6cd7d6746ef935c3824770b1bc8a414899f9
Anton Hagström <antonhagstrom1408@gmail.com>
2025-09-11 11:47:45 +0200

    index

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 # mrating
 
 echo "hello";
-
+<form action="insert.php" method="POST">
+    Movie Name:<input type="text" name="mname"><br>
+    Movie Year:<input type="text" name="myear"><br>
+    Movie Genre:<input type="text" name="mgenre"><br>       #dropdown
+    Movie Rating:<input type="text" name="mrating"><br>
+    <input type="submit" value="Add">
+</form>
 ?>
